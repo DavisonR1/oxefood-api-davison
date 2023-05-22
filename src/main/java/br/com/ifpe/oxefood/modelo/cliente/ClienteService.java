611fc75bd8f5347f3d89d3e5930ec7f345dff60e
unknown <drl1@discente.ifpe.br>
2023-05-22 19:43:24 -0300

    API incluindo o UPDATE

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -22,13 +22,27 @@ public class ClienteService extends GenericService {
        return repository.save(cliente);
    }
 
-public List<Cliente> listarTodos() {
-    return repository.findAll();
-}
+    public List<Cliente> listarTodos() {
+        return repository.findAll();
+    }
 
-public Cliente obterPorID(Long id) {
-    return repository.findById(id).get();
-}
+    public Cliente obterPorID(Long id) {
+        return repository.findById(id).get();
+    }
 
+    @Transactional
+    public void update(Long id, Cliente clienteAlterado) {
+
+        Cliente cliente = repository.findById(id).get();
+        cliente.setNome(clienteAlterado.getNome());
+        cliente.setDataNascimento(clienteAlterado.getDataNascimento());
+        cliente.setCpf(clienteAlterado.getCpf());
+        cliente.setFoneCelular(clienteAlterado.getFoneCelular());
+        cliente.setFoneFixo(clienteAlterado.getFoneFixo());
+            
+        super.preencherCamposAuditoria(cliente);
+        repository.save(cliente);
+        
+    }
 }
 
